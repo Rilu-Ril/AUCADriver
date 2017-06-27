9af20e579f63599ee122a52dbef5a94d66ec414b
Sanria Madzhikova <sanira.ril@gmail.com>
2017-06-27 14:40:28 +0600

    #1 setup login controllers

---
+++ AUCADriver/Controllers/PassengerLoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  PassengerLoginVC.swift
+//  AUCADriver
+//
+//  Created by Sanira on 6/27/17.
+//  Copyright © 2017 iCoder. All rights reserved.
+//
+
+import UIKit
+
+class PassengerLoginVC: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassowrd: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func login() {
+        
+    }
+    
+    @IBAction func signIn() {
+        
+    }
+
+}
---
+++ AUCADriver/Plugins/AuthProvider.swift
@@ -0,0 +1,13 @@
+//
+//  AuthProvider.swift
+//  AUCADriver
+//
+//  Created by Sanira on 6/27/17.
+//  Copyright © 2017 iCoder. All rights reserved.
+//
+
+import Foundation
+
+class AuthProvider {
+    
+}
